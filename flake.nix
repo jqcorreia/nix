9951873815287173483f826705e4b4b5a508fec5
José Quadrado <joseqcorreia@gmail.com>
2024-11-07 00:45:19 +0000

    Add system packages module.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,39 @@
 {
-    inputs = { 
-        unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      legacyPackages =
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "x86_64-darwin"
+          ]
+          (
+            system:
+            import inputs.unstable {
+              inherit system;
+              # NOTE: Using `nixpkgs.config` in your NixOS config won't work
+              # Instead, you should set nixpkgs configs here
+              # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
 
-    outputs = { self, nixpkgs, ...}@inputs:
-        let 
-            system = "x86_64-linux";
-            legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
-              import inputs.unstable {
-                inherit system;
-                # NOTE: Using `nixpkgs.config` in your NixOS config won't work
-                # Instead, you should set nixpkgs configs here
-                # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
+              config.allowUnfree = true;
+            }
+          );
 
-                config.allowUnfree = true;
-              }
-            );
-
-        in
-        {
-            nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-                specialArgs = { unstable = legacyPackages.x86_64-linux; };
-                modules = [
-                    ./configuration.nix
-                ];
-            };
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          unstable = legacyPackages.x86_64-linux;
         };
+        modules = [
+          ./configuration.nix
+          ./system-packages.nix
+        ];
+      };
+    };
 }
-
---
+++ system-packages.nix
@@ -0,0 +1,41 @@
+{ pkgs, unstable, ... }:
+{
+  config = {
+    environment.systemPackages = with pkgs; [
+      bat
+      wget
+      chezmoi
+      git
+      git-lfs
+      alacritty
+      tmux
+      nodejs
+      jq
+      waybar
+      rclone
+      fzf
+      z-lua
+      gnupg
+      gcc
+      go
+      pass
+      lshw
+      unzip
+      pavucontrol
+      networkmanagerapplet
+      pritunl-client
+      SDL2
+      SDL2_ttf
+      SDL2_image
+      ripgrep
+      lua-language-server
+      python312
+      unstable.ruff-lsp
+      unstable.pyright
+      unstable.neovim
+      vivaldi
+      nil
+      nixfmt-rfc-style
+    ];
+  };
+}
