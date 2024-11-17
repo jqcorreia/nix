0d27d0ac3be8022d18a6e72d14cf7c5eda3dc543
José Quadrado <joseqcorreia@gmail.com>
2024-11-17 22:03:36 +0000

    README update and minor changes.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # nix
+
+# Laptop (vertex)
+
+Problems:
+- Whatsapp get funky after browser restart
+- SDDM asks second time for the password after a reboot, probably PAM
+- icon theme not set, dunno why
+- sometimes blueman manager doesn't open from the applet due to, it seems, some dbus stuff that needs to happen
+- Unstable errors:
+    - vivaldi
+    - hyprland
+
+Solved:
+- using nix-shell inside a rust project with rust analizer changes env vars resulting in recompilation of packages (e.g ring and rusttls). Installing the packages `cargo` and `rust-analyzer` inside the shell solves this. Should not use `rustup`
+
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,22 +33,8 @@
     awscli2
     feh
     smplayer
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    cargo
+    rust-analyzer
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -76,6 +62,7 @@
       };
     };
   };
+
   gtk = {
     enable = true;
     iconTheme = {
